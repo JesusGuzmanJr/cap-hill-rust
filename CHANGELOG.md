615bc99f03a61955e063a165baff66205f0843d0
Jesus Guzman, Jr <jesus@marzipan.club>
2023-10-16 05:49:34 -0700

    switch from docker and fly to systemd service

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,27 +5,26 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.2.0-rc.1] - 2023-10-13
+## [0.2.0-rc.1] - 2023-10-16
 
 ### Added
 
 - Added prototype library page.
 - Added serving of static files.
 - Redirect http to https.
-- Added 404 page.
 - Added favicons.
 - Add robots.txt.
-- Added Postgres support.
-- Switching to Leptos and unstable rust.
-- Switch to nightly-2023-10-01.
 
 ### Fixed
 
 ### Changed
-
-- Refactored pages to use askama templates.
+- Use systemd for service management.
+- Switching to Leptos and unstable rust.
+- Switch to nightly-2023-10-01.
 
 ### Removed
+- Dockerfile and fly.toml.
+- Postgres
 
 ## [0.1.0] - 2023-10-08
 
--- server_setup.sh
+++ server_setup.sh
@@ -9,15 +9,20 @@ usermod -s $(which zsh) root
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+exit
+
+# install zsh-syntax-highlighting
+cd /root/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:--/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# install zsh-autosuggestions
+cd /root/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:--/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-# install zsh-syntax-highlighting
-cd /root/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# install zsh-autosuggestions
-cd /root/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# install bottom
+dnf copr enable atim/bottom -y
+dnf install bottom -y
 
 # install leptos
 cargo install cargo-leptos --version 0.2.0
